4c05ca7a3d52ac7711c96991a9e414f2fe168810
James Thompson <112099604+jthompsonnait@users.noreply.github.com>
2026-03-06 08:16:09 -0700

    March 6 -1

---
+++ HogWild/HogWildSystem/BLL/CustomerService.cs
@@ -0,0 +1,26 @@
+﻿using HogWildSystem.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HogWildSystem.BLL
+{
+    public class CustomerService
+    {
+        #region Fields
+        //  hog wild context
+        private readonly HogWildContext _hogWildContext;
+        #endregion
+
+
+        //  Constructor for the WorkingVersionService CLass
+        internal CustomerService(HogWildContext hogWildContext)
+        {
+            //  initialize the _hogWildContext field with the provided
+            //      HogWildContext instance
+            _hogWildContext = hogWildContext;
+        }
+    }
+}
---
+++ HogWild/HogWildSystem/ViewModels/CustomerSearchView.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HogWildSystem.ViewModels
+{
+    public class CustomerSearchView
+    {
+        //  Customer ID
+        public int CustomerID { get; set; }
+        //	First Name
+        public string FirstName { get; set; }
+        //	Last Name
+        public string LastName { get; set; }
+        //	City
+        public string City { get; set; }
+        //	contact phone number
+        public string Phone { get; set; }
+        //	email address
+        public string Email { get; set; }
+        //	status ID.	Status value will uswe a dropdown and a "LookupView" Model
+        public int StatusID { get; set; }
+        //	Invoice.SubTotal + Invoice.Tax
+        public decimal TotalSales { get; set; } = 0;
+    }
+}
